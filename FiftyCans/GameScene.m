77836b9fbdcf201697eaaf7619cd5dd8d3f80fe0
Aaron Schachter <aaronschachter@gmail.com>
2015-05-15 14:10:48 -0700

    Adds flying cans, shoots lasers

--- FiftyCans/GameScene.m
+++ FiftyCans/GameScene.m
@@ -8,23 +8,83 @@
 
 #import "GameScene.h"
 
+@interface GameScene()
+@property (nonatomic) SKSpriteNode *player;
+@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
+@end
+
+static inline CGPoint rwAdd(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint rwSub(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint rwMult(CGPoint a, float b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline float rwLength(CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+// Makes a vector have a length of 1
+static inline CGPoint rwNormalize(CGPoint a) {
+    float length = rwLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+
+    self.backgroundColor = [SKColor colorWithRed:0.1 green:0.1 blue:0.1 alpha:1.0];
+    self.player = [SKSpriteNode spriteNodeWithImageNamed:@"console-color"];
+    self.player.position = CGPointMake(200, 150);
+    [self addChild:self.player];
+    [self addCan];
+}
+
+- (void)addCan {
+    SKSpriteNode *can = [SKSpriteNode spriteNodeWithImageNamed:@"can-tuna"];
+    // Determine where to spawn the monster along the Y axis
+    int minY = can.size.height / 2;
+    int maxY = self.frame.size.height - can.size.height / 2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+
+    // Create the monster slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    can.position = CGPointMake(self.frame.size.width + can.size.width/2, actualY);
+    [self addChild:can];
+
+    // Determine speed of the monster
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+
+    // Create the actions
+    SKAction * actionMove = [SKAction moveTo:CGPointMake(-can.size.width/2, actualY) duration:actualDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+    [can runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+}
+
+- (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
+
+    self.lastSpawnTimeInterval += timeSinceLast;
+    if (self.lastSpawnTimeInterval > 1) {
+        self.lastSpawnTimeInterval = 0;
+        [self addCan];
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
+    return;
+
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
@@ -42,8 +102,51 @@
     }
 }
 
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+
+    // 1 - Choose one of the touches to work with
+    UITouch * touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+
+    // 2 - Set up initial location of projectile
+    SKSpriteNode * projectile = [SKSpriteNode spriteNodeWithImageNamed:@"laser"];
+    projectile.position = self.player.position;
+
+    // 3- Determine offset of location to projectile
+    CGPoint offset = rwSub(location, projectile.position);
+
+    // 5 - OK to add now - we've double checked position
+    [self addChild:projectile];
+
+    // 6 - Get the direction of where to shoot
+    CGPoint direction = rwNormalize(offset);
+
+    // 7 - Make it shoot far enough to be guaranteed off screen
+    CGPoint shootAmount = rwMult(direction, 1000);
+
+    // 8 - Add the shoot amount to the current position
+    CGPoint realDest = rwAdd(shootAmount, projectile.position);
+
+    // 9 - Create the actions
+    float velocity = 480.0/1.0;
+    float realMoveDuration = self.size.width / velocity;
+    SKAction * actionMove = [SKAction moveTo:realDest duration:realMoveDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+    [projectile runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+    
+}
+
 -(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+    // Handle time delta.
+    // If we drop below 60fps, we still want everything to move the same distance.
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    if (timeSinceLast > 1) { // more than a second since last update
+        timeSinceLast = 1.0 / 60.0;
+        self.lastUpdateTimeInterval = currentTime;
+    }
+
+    [self updateWithTimeSinceLastUpdate:timeSinceLast];
 }
 
 @end
